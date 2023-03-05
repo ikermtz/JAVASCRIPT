c52c27d23ef83e779649e57c2736c29d619080a9
ikermtz <49227870+ikermtz@users.noreply.github.com>
2023-03-05 17:51:55 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,79 +1,105 @@
-function inicializarGestores(){
 
-    var imagenes = ["carne.jpg", "drink.jpg", "fiorentina.jpg", "heade_ft.jpg"];
-    var i = 0;
+function inicializarGestores()
+{
+	var imagen = document.getElementById("imagen");
+	var reloj = setInterval(cambioFondo,5000);	// Gestión del evento que llama a la función que cambia el fondo
 
-    var reloj = setInterval(cambiar, 5000);
-    var imagen = document.getElementById("imagen");
+	var i = 0;	// Índice para rotar las imágenes del fondo
+	var imagenes = ["../imagenes/fresas.jpg",	//Imágenes para rotar
+		"../imagenes/limon.jpg",
+		"../imagenes/mandarinas.jpg",
+		"../imagenes/manzanas.jpg",
+		"../imagenes/melon.jpg",
+		"../imagenes/sesamo.jpg"];
 
-    imagen.addEventListener("click", () => {
-    console.log("Intervalo detenido")
-    clearInterval(reloj);}, false);
 
-    function cambiar(){
-        if (imagenes.length == i) {
-            i = 0;
-        }
-        imagen.style.backgroundImage = "url(images/"+imagenes[i]+")";
-        console.log("La nueva imagen es: "+imagenes[i])
-        i = i +1;
-    }
+	// Añadir un listener para llamar al método de imprimir las teclas cuando el usuario tenga el foco en la ventana del navegador
+	window.addEventListener('focus', imprimirTecla, false);
 
-	document.addEventListener("keydown", flechas)
+	function imprimirTecla(){
+		document.addEventListener("keydown", function (event) {
 
-	function flechas(event) {
-	    var x = window.scrollX;
-		var y = window.scrollY;
-        const tecla = event.key.toLowerCase();
+			// Obtener las coordenadas dactuales del scroll
+			var x = window.scrollX;
+			var y = window.scrollY;
 
-        if (tecla == 'arrowright') {
-            alert("Flecha derecha pulsada");
-        }
-        else if (tecla == 'arrowleft') {
-            alert("Flecha izquierda pulsada");
-        }
-        else if (tecla == 'arrowup') {
-            alert("Flecha arriba pulsada");
-        }
-        else if (tecla == 'arrowdown') {
-            alert("Flecha abajo pulsada");
-        }
+			// Obtener los eventos de las teclas pulsadas
+			const keyName = event.key;
+
+			// Comprobar que las teclas coincidan con las flechas del teclado
+			switch(keyName){
+				case "ArrowRight":
+					alert("Has pulsado la flecha derecha");
+					break;
+				case "ArrowLeft":
+					alert("Has pulsado la flecha izquierda");
+					break;
+				case "ArrowUp":
+					alert("Has pulsado la flecha de arriba");
+					break;
+				case "ArrowDown":
+					alert("Has pulsado la flecha de abajo");
+					break;
+			}
+		}, { once: true });	// Ejecutar sólo una vez
 	}
 
-	var imagen = document.getElementById("imagen");
-      imagen.onclick = function () {
-        alert("Has pulsado la imagen");
-      };
+	// Bloquear el scroll al pulsar cualquiera de las flechas
+	window.addEventListener("keydown", function(e) {
+		if(["ArrowRight","ArrowLeft","ArrowUp","ArrowDown"].indexOf(e.code) > -1) {
+			e.preventDefault();
+		}
+	}, false);
 
-      var usuario = document.getElementById("usuario");
-      usuario.value = "tu@email";
 
-      usuario.onblur = function () {
-        if (usuario.value == "") {
-          usuario.value = "tu@email";
-        }
-      };
+	imagen.onclick = function()
+	{
+		alert("Has pulsado la imagen");
 
-      usuario.onfocus = function () {
-        if (usuario.value == "tu@email") {
-          usuario.value = "";
-        }
-      };
+		// Borrar evento del intervalo
+		clearInterval(reloj);
+	}
 
-      var item = document.getElementById("combobox");
-      item.addEventListener("change", gestorCombo);
+	var usuario = document.getElementById("usuario");
+	usuario.value = 'tu@email';
 
-      function gestorCombo() {
-        console.log(item.value);
-        console.log(item.options[item.selectedIndex].text);
-        console.log(item.selectedIndex);
-      }
+	usuario.onblur = function(){
+		if (usuario.value == ''){
+			usuario.value = "tu@email";
+		}
+	}
 
-      var formulario = document.getElementById("formulario");
-      formulario.onsubmit = function () {
-        console.log("click en submit");
-        return false;
-      };
-    }
+	usuario.onfocus = function(){
+		if (usuario.value == 'tu@email'){
+			usuario.value = '';
+		}
+	}
 
-    window.onload = inicializarGestores;
+	var item = document.getElementById("combobox");
+	item.addEventListener("change",gestorCombo);
+
+	function gestorCombo(){
+		console.log(item.value);
+		console.log(item.options[item.selectedIndex].text);
+		console.log(item.selectedIndex);
+	} 
+
+	var formulario = document.getElementById('formulario');
+	formulario.onsubmit = function(){
+		console.log("click en submit");
+		return false;
+	}
+
+	// Función para cambiar el fondo
+	function cambioFondo() {
+		if(i>=0 && i < 6){
+			imagen.style.backgroundImage = "url("+imagenes[i]+")";
+			i++;
+		}
+		else{
+			i = 0;
+		}
+	}
+}
+
+window.onload = inicializarGestores;
